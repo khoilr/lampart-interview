3c6577c6a3f27105878dcd62075e667cc2703bd7
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2022-03-16 21:56:43 +0700

    add composer.lock

--- App/Config.php
+++ App/Config.php
@@ -14,25 +14,25 @@ class Config
      * Database host
      * @var string
      */
-    const DB_HOST = 'localhost';
+    const DB_HOST = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 
     /**
      * Database name
      * @var string
      */
-    const DB_NAME = 'lampart';
+    const DB_NAME = 'e2ol4lf91s6greyi';
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'root';
+    const DB_USER = 'plinwopff9fcaa6n';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'khoikhoi';
+    const DB_PASSWORD = 'ctgkef9c7pyhfdrv';
 
     /**
      * Show or hide error messages on screen
